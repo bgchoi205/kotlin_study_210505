ebd4b514e5aa63f4df8315b3bb0ffd215f230ec1
bgchoi205 <choibg205@naver.com>
2021-05-12 11:07:34 +0900

    Json 파일 저장하기

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -4,6 +4,36 @@ fun main(){
 
     testWriteFile()
 
+    testWriteFile2()
+
+}
+
+data class TestArticle(
+    val id : Int,
+    val title : String,
+    val body : String
+) {
+    fun toJson(): String {
+        var jsonStr = ""
+
+        jsonStr += "{"
+        jsonStr += "\r\n"
+        jsonStr += "\t" + """ "id":$id """.trim() + ","
+        jsonStr += "\r\n"
+        jsonStr += "\t" + """ "title":"$title" """.trim() + ","  // 값이 문자열이면 "로 다시 감싸준다
+        jsonStr += "\r\n"
+        jsonStr += "\t" + """ "body":"$body" """.trim()
+        jsonStr += "\r\n"
+        jsonStr += "}"
+
+        return jsonStr
+    }
+}
+
+fun testWriteFile2() {
+    val testArticle = TestArticle(1, "제목1", "내용1")
+
+    File("test/3.json").writeText(testArticle.toJson())
 }
 
 fun testWriteFile(){
